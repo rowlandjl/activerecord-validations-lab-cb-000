959a897558dc105bcb9f43db152f11cf160045a5
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-01 17:49:23 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -14,7 +14,7 @@ class Post < ActiveRecord::Base
     
     def is_clickbait?
       if CLICKBAIT_PATTERNS.none? { |pattern| pattern.match title }
-        errors.add(title: "must be clickbait")
+        errors.add(:title, "must be clickbait")
       end 
     end 
 end
