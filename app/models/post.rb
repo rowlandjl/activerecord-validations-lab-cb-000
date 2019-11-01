1e0c4bb98eb28855a369632472c0fc3851d8d5fe
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-01 17:45:17 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,6 @@ class Post < ActiveRecord::Base
   validates :summary, length: { maximum: 250 }
   validates :content, length: { minimum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  
+  CLICKBAIT_PATTERNS = 
 end
