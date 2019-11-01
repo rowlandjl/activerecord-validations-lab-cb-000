e33c641ecce4d16c074d21ce731767c6a1ebd8b2
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-01 17:40:46 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ActiveRecord::Base
   validates :title, presence: true 
   validates :summary, length: { maximum: 250 }
   validates :content, length: { minimum: 250 }
-  validates :category, inclusion: 
+  validates :category, inclusion: { in: (Fiction Non-Fiction) }
 end
